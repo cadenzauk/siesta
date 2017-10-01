28f01ae9a55f18a0681705cca4cc7fef3cfcd5ee
mdrodg <mark.rodgers@cadenza.co.nz>
2017-10-01 13:44:25 +0100

    Fix Postgres minute diff function.

--- siesta/src/main/java/com/cadenzauk/siesta/dialect/PostgresDialect.java
+++ siesta/src/main/java/com/cadenzauk/siesta/dialect/PostgresDialect.java
@@ -33,29 +33,34 @@ import com.cadenzauk.siesta.grammar.expression.TypedExpression;
 import com.cadenzauk.siesta.type.DbTypeId;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 import static com.cadenzauk.core.lang.StringUtil.octal;
 import static com.cadenzauk.siesta.dialect.function.date.DateFunctionSpecs.HOUR_DIFF;
+import static com.cadenzauk.siesta.dialect.function.date.DateFunctionSpecs.MINUTE_DIFF;
 
 public class PostgresDialect extends AnsiDialect {
     public PostgresDialect() {
         DateFunctionSpecs.registerExtract(functions());
         DateFunctionSpecs.registerPlusNumber(functions());
-        functions().register(DateFunctionSpecs.CURRENT_TIMESTAMP, ArgumentlessFunctionSpec.of("localtimestamp"));
-        functions().register(HOUR_DIFF, new FunctionSpec() {
-            @Override
-            public String sql(String[] argsSql) {
-                return String.format("date_part('day', %1$s - %2$s) * 24 + date_part('hour', %1$s - %2$s)", argsSql[0], argsSql[1]);
-            }
+        functions()
+            .register(DateFunctionSpecs.CURRENT_TIMESTAMP, ArgumentlessFunctionSpec.of("localtimestamp"))
+            .register(HOUR_DIFF, new FunctionSpec() {
+                @Override
+                public String sql(String[] argsSql) {
+                    return String.format("date_part('day', %1$s - %2$s) * 24 + date_part('hour', %1$s - %2$s)", argsSql[0], argsSql[1]);
+                }
 
-            @Override
-            public Stream<Object> args(Scope scope, TypedExpression<?>[] args) {
-                return Stream.concat(
-                    Arrays.stream(args),
-                    Arrays.stream(args)).flatMap(a -> a.args(scope));
-            }
-        });
+                @Override
+                public Stream<Object> args(Scope scope, TypedExpression<?>[] args) {
+                    return Stream.concat(
+                        Arrays.stream(args),
+                        Arrays.stream(args)).flatMap(a -> a.args(scope));
+                }
+            })
+            .register(MINUTE_DIFF, argsSql -> String.format("extract(epoch from (date_trunc('minute', %1$s) - date_trunc('minute', %2$s))) / 60", argsSql[0], argsSql[1]))
+        ;
 
         types()
             .register(DbTypeId.TINYINT, new DefaultTinyint("smallint"))
@@ -86,6 +91,25 @@ public class PostgresDialect extends AnsiDialect {
     @Override
     public String fetchFirst(String sql, long n) {
         return String.format("%s offset 0 rows fetch next %d rows only", sql, n);
+    }
+
+    @Override
+    public boolean supportsLockTimeout() {
+        return true;
+    }
+
+    @Override
+    public String setLockTimeout(long time, TimeUnit unit) {
+        long millis = unit.toMillis(time);
+        if (millis == 0) {
+            millis = 1;
+        }
+        return String.format("SET LOCAL lock_timeout = '%dms'", millis);
+    }
+
+    @Override
+    public String resetLockTimeout() {
+        return "SET LOCAL lock_timeout = '10s'";
     }
 
     @Override
